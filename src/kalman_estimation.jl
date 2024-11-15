187f525f45d08ed8c87d9ed7ee842873c8e6cecc
Shashank Swaminathan <shswami@mit.edu>
2024-11-15 17:54:19 -0500

    Adding computation of info priors and innovation

--- src/kalman_estimation.jl
+++ src/kalman_estimation.jl
@@ -1,3 +1,6 @@
+using Parameters: @unpack
+using Combinatorics: combinations
+
 export AgentEnvModel, initialize_agent, next_agent_state
 export SystemEstimators, simple_LGSF_Estimators
 
@@ -57,6 +60,16 @@ end
 
 This is a linearized representation.
 The mutating elements are the vectors, which are appended to.
+
+The timing of this is slightly unintuitive.
+* Estimates carry two properties - the estimate of the system, and the observation.
+    * System estimates are our estimates at time (k) **before** taking observations.
+    * Observations are taken at time (k), but are not fused into the estimates until (k+1).
+* Information carry two properties - the information values and the innovations.
+    * Both are calculated **after** taking the observation at time (k).
+    * The innovations values represent the innovation gained after observation at time (k).
+    * The information values represent the updated information about the state after observation at time (k).
+    * Accordingly, the system estimate at time (k+1) will be recovered from the information at time (k).
 """
 mutable struct AgentEnvModel
     k::Integer
@@ -75,25 +88,85 @@ function initialize_agent(params::SCRIBEModelParameters, bhv::SCRIBEObserverBeha
     AgentEnvModel(1, cwrld.k-1, params, bhv, [init_agent_estimate(cwrld, 1, params, bhv, X₀)], [init_agent_info(params.nᵩ)])
 end
 
-function next_agent_state(agent::AgentEnvModel, ϕₖ::Vector{Float64}, cwrld::SCRIBEModel, X::Matrix{Float64})
-    push!(agent.estimates, new_agent_estimate(cwrld, agent.k+1, agent.estimates[agent.k].estimate, ϕₖ, agent.bhv, X))
-end
-
 struct SystemEstimators
     system::AgentEnvModel
     A::Function
     ϕ::Function
+    Q::Function
     H::Function
     z::Function
+    R::Function
+    Y::Function
+    y::Function
 
-    SystemEstimators(system::AgentEnvModel, A::Function, ϕ::Function, H::Function, z::Function) = new(system, A, ϕ, H, z)
+    SystemEstimators(system::AgentEnvModel, A::Function, ϕ::Function, Q::Function,
+                     H::Function, z::Function, v::Function,
+                     Y::Function, y::Function) = new(system, A, ϕ, Q, H, z, R, Y, y)
 end
 
 function simple_LGSF_Estimators(system::AgentEnvModel)
     get_A(k, system) = system.estimates[k].estimate.params.A
     get_ϕ(k, system) = system.estimates[k].estimate.ϕ
+    get_Q(k, system) = system.params.w[:Q]
     get_H(k, system) = compute_obs_dynamics(system.estimates[k].estimate, system.estimates[k].observations.X)
     get_z(k, system) = system.estimates[k].observations.z
+    get_R(k, system) = system.estimates[k].observations.v[:R]
+    get_Y(k, system) = system.information[k].Y
+    get_y(k, system) = system.information[k].y
 
-    SystemEstimators(system, kA->get_A(kA, system), kϕ->get_ϕ(kϕ, system), kH->get_H(kH, system), kz->get_z(kz, system))
+    SystemEstimators(system, kA->get_A(kA, system), kϕ->get_ϕ(kϕ, system), kQ->get_Q(kQ, system),
+                     kH->get_H(kH, system), kz->get_z(kz, system), kR->get_R(kR, system),
+                     kY->get_Y(kY, system), ky->get_y(ky, system))
+end
+
+function next_agent_state(agent::AgentEnvModel, ϕₖ::Vector{Float64}, cwrld::SCRIBEModel, X::Matrix{Float64})
+    push!(agent.estimates, new_agent_estimate(cwrld, agent.k+1, agent.estimates[agent.k].estimate, ϕₖ, agent.bhv, X))
+end
+
+"""Computes the prior update **of the next step** Y⁻(k+1).
+
+Takes two inputs:
+* Estimator functions (`Ef::SystemEstimators`)
+* The **current** timestep `k`. It will use this along `Ef` to lookup corresponding system information.
+"""
+function compute_info_priors(Ef::SystemEstimators, k::Integer)
+    @unpack _, A, _, Q, H, _, _, Y, y = Ef
+    M  = inv(A(k))' * Y(k-1) * inv(A(k))
+    C  = M + inv(Q(k))
+    Y⁻ = M - M * inv(C) * M
+    y⁻ = Y⁻ * A(k) * Y(k-1) * y(k-1)
+    return Y⁻, y⁻
+end
+
+"""Computes the current innovation gained by observation at time k.
+
+Takes two inputs:
+* Estimator functions (`Ef::SystemEstimators`)
+* The **current** timestep `k`. It will use this along `Ef` to lookup corresponding system information.
+"""
+function compute_innov_from_obs(Ef::SystemEstimators, k::Integer)
+    @unpack _, _, _, _, H, z, R, _, _ = Ef
+    δI = H(k)' * inv(R(k)) * H(k)
+    δi = H(k)' * inv(R(k)) * z(k)
+    return δI, δi
+end
+
+function centralized_fusion(agent_estimators::Vector{SystemEstimators}, k::Integer)
+    # Compute priors from current system state at t=k and previous information state at t=k-1
+    priors = map(ef->compute_info_priors(ef, k), agent_estimators)
+
+    # Ensure all priors are the same
+    if size(priors,1)>1
+        for prior_pair in combinations(priors, 2)
+            let prior_a=prior_pair[1], prior_b=prior_pair[2]
+                @assert isapprox(prior_a[1], prior_b[1]) "Information matrix priors for Y⁻(k+1) are diverged!"
+                @assert isapprox(prior_a[2], prior_b[2]) "Information value priors for y⁻(k+1) are diverged!"
+            end
+        end
+    end
+
+
+    # Compute innovations for t=k+1 from current observation at t=k
+    innovs = map(ef->compute_innov_from_obs(ef, k), agent_estimators)
+    avg_innov = average(innovs)
 end
