1c1ee4f7855da876961e25ed84562944343bf2ac
Shashank Swaminathan <shswami@mit.edu>
2024-11-19 23:16:49 -0500

    Big debugging shifts ...

--- test/test_kalman.jl
+++ test/test_kalman.jl
@@ -22,13 +22,18 @@ function test_agent_setup()
     return gt_params, gt_model, sample_locations, observer, observations
 end
 
-function quick_setup(; testing=true)
-    ϕ₀=[-0.5,0.5,0.75,0.5,-0.5]
-    gt_params=LGSFModelParameters(μ=hcat(range(-1,1,5), zeros(5)),σ=[1.],τ=[1.],ϕ₀=ϕ₀,A=nothing,Q=nothing)
+function quick_setup(nᵩ=2; testing=true)
+    if nᵩ==2
+        ϕ₀=[-0.5,0.5]
+    else
+        ϕ₀=[-0.5,0.5,0.75,0.5,-0.5]
+    end
+
+    gt_params=LGSFModelParameters(μ=hcat(range(-1,1,nᵩ), zeros(nᵩ)),σ=[1.],τ=[1.],ϕ₀=ϕ₀,A=nothing,Q=nothing)
     gt_model=[initialize_SCRIBEModel_from_parameters(gt_params)]
     
-    ag_params=LGSFModelParameters(μ=hcat(range(-1,1,5), zeros(5)),σ=[1.],τ=[1.],
-                                  ϕ₀=zeros(size(ϕ₀,1)),A=Matrix{Float64}(I(size(ϕ₀,1))),Q=Matrix{Float64}(I(size(ϕ₀,1))))
+    ag_params=LGSFModelParameters(μ=hcat(range(-1,1,nᵩ), zeros(nᵩ)),σ=[1.],τ=[1.],
+                                  ϕ₀=zeros(nᵩ), A=Matrix{Float64}(I(nᵩ)), Q=Matrix{Float64}(I(nᵩ)))
     observer=LGSFObserverBehavior(0.3)
     sample_locations=[]
     init_agent_loc=[0. 0.;]
@@ -42,20 +47,7 @@ function quick_setup(; testing=true)
 end
 
 function test_estimators(; testing=true)
-    ϕ₀=[-0.5,0.5,0.75,0.5,-0.5]
-    gt_params=LGSFModelParameters(μ=hcat(range(-1,1,5), zeros(5)),σ=[1.],τ=[1.],ϕ₀=ϕ₀,A=nothing,Q=nothing)
-    gt_model=[initialize_SCRIBEModel_from_parameters(gt_params)]
-    
-    ag_params=LGSFModelParameters(μ=hcat(range(-1,1,5), zeros(5)),σ=[1.],τ=[1.],
-                                  ϕ₀=zeros(size(ϕ₀,1)),A=Matrix{Float64}(I(size(ϕ₀,1))),Q=Matrix{Float64}(I(size(ϕ₀,1))))
-    observer=LGSFObserverBehavior(0.3)
-    sample_locations=[]
-    init_agent_loc=[0. 0.;]
-    push!(sample_locations, init_agent_loc)
-    ag=initialize_agent(ag_params, observer, gt_model[1], init_agent_loc)
-    if testing; @test typeof(ag) == AgentEnvModel; end
-
-    lg_Fs=simple_LGSF_Estimators(ag)
+    (ϕ₀, gt_params, gt_model, ag_params, observer, sample_locations, ag, lg_Fs) = quick_setup(5, testing=testing)
 
     if testing
         @test lg_Fs.ϕ(1)==zeros(5)
@@ -69,34 +61,34 @@ function test_estimators(; testing=true)
         @test_throws BoundsError lg_Fs.z(2)
     end
 
-    # Add temporary lexical scoping for stupid testing
-    let gt_model=deepcopy(gt_model), sample_locations=deepcopy(sample_locations), ag=deepcopy(ag)
-        lg_Fs=simple_LGSF_Estimators(ag)
-
-        for i in 1:4
-            push!(gt_model, update_SCRIBEModel(gt_model[i]))
-            push!(sample_locations, rand(2,2))
-            next_agent_state(ag,zeros(ag_params.nᵩ), gt_model[i+1], sample_locations[i+1])
-        end
-        
-        if testing
-            @test_throws BoundsError lg_Fs.ϕ(6)
-            @test_throws BoundsError lg_Fs.A(6)
-            @test_throws BoundsError lg_Fs.H(6)
-            @test_throws BoundsError lg_Fs.z(6)
-
-            @test all([isapprox(lg_Fs.ϕ(i), zeros(5)) for i in 1:5])
-            @test !all([size(lg_Fs.H(i))==(2,5) for i in 1:5])
-            @test all([size(lg_Fs.H(i))==(2,5) for i in 2:5])
-            @test all([size(lg_Fs.z(i))==(2,) for i in 2:5])
-        end
+    for i in 1:4
+        push!(gt_model, update_SCRIBEModel(gt_model[i]))
+        push!(sample_locations, rand(2,2))
+        next_agent_state(ag,zeros(ag_params.nᵩ), gt_model[i+1], sample_locations[i+1])
     end
+        
+    if testing
+        @test_throws BoundsError lg_Fs.ϕ(6)
+        @test_throws BoundsError lg_Fs.A(6)
+        @test_throws BoundsError lg_Fs.H(6)
+        @test_throws BoundsError lg_Fs.z(6)
 
-    fused_info=[ag.information[1]]
+        @test all([isapprox(lg_Fs.ϕ(i), zeros(5)) for i in 1:5])
+        @test !all([size(lg_Fs.H(i))==(2,5) for i in 1:5])
+        @test all([size(lg_Fs.H(i))==(2,5) for i in 2:5])
+        @test all([size(lg_Fs.z(i))==(2,) for i in 2:5])
+    end
+end
+
+function test_centralized_KF(; testing=true)
+    (ϕ₀, gt_params, gt_model, ag_params, observer, sample_locations, ag, lg_Fs) = quick_setup(2, testing=testing)
+    nᵩ=ag_params.nᵩ # storing for easier debugging
+
+    fused_info=Any[ag.information[1]]
     for i in 1:100
         push!(fused_info,centralized_fusion([lg_Fs], i)[1])
         push!(gt_model, update_SCRIBEModel(gt_model[i]))
-        push!(sample_locations, 3*rand(5,2))
+        push!(sample_locations, 3*rand(2,2))
         progress_agent_env_filter(ag, fused_info[end], gt_model[i+1], sample_locations[i+1])
         # next_agent_state(ag,zeros(ag_params.nᵩ), gt_model[i+1], sample_locations[i+1])
         # next_agent_time(ag)
@@ -112,4 +104,8 @@ end
 
 @testset "Single Agent Estimators" begin
     test_estimators()
+end
+
+@testset "Centralized Kalman Filter" begin
+    test_centralized_KF()
 end
