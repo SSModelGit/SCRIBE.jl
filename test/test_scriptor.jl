92b1fc000a91ac1bd5819f7776e5fde1c9e80cce
Shashank Swaminathan <shswami@mit.edu>
2025-02-03 22:20:16 -0500

    First attempt at scripter

--- test/test_scriptor.jl
+++ test/test_scriptor.jl
@@ -3,6 +3,7 @@ using SCRIBE
 
 using Match: @match
 using LinearAlgebra: I, norm
+using Combinatorics: combinations
 
 using JLD2: @save, @load
 using Plots: heatmap, plot, plot!, savefig
@@ -12,7 +13,24 @@ using Plots: heatmap, plot, plot!, savefig
 In the case of multiple agents, the only differences will be in initial locations.
 All agents will start at [0. 0.].
 """
-function quick_setup(agent_ids, agent_conns, nᵩ; testing=true)
+function quick_setup(agent_ids, agent_conns;
+                     gt_desc=(:gsf, Dict([(:nᵩ, 3), (:τ, 1.), (:σ, 1.)])),
+                     ag_desc = Dict([(:gt_same, true), (:σ, 1.), (:τ, 1.), (:μ, [-0.5 0; 0. 0.; 0.5 0.]), (:nᵩ, 3)]))
+    @match gt_desc[1] begin
+        :gsf => return quick_GSF(agent_ids, agent_conns, gt_desc[2]; ag_desc = ag_desc)
+    end
+end
+
+function create_grid_gsf_μστ(space_corners=[-5., 5], well_spacing=0.5; τ=1.0, σ=0.5)
+    μ=reduce(vcat, transpose.(collect(combinations(space_corners[1]:well_spacing:space_corners[2]))))
+    return [(:gt_same, false), (:σ, 1.), (:τ, 1.), (:μ, μ), (:nᵩ, size(μ,1))]
+end
+
+function quick_GSF(agent_ids, agent_conns, gt_desc=Dict([(:nᵩ, 3), (:τ, 1.), (:σ, 1.)]);
+                   ag_desc=Dict([(:gt_same, true), (:σ, 1.), (:τ, 1.), (:μ, [-0.5 0; 0. 0.; 0.5 0.]), (:nᵩ, 3)]))
+    nᵩ = gt_desc[:nᵩ]
+    σ = gt_desc[:σ]
+    τ = gt_desc[:τ]
     (ϕ₀, μ) = @match nᵩ begin
         2 => ([-0.5,0.5], hcat(range(-1,1,nᵩ), zeros(nᵩ)))
         3 => ([-0.5, 0.5, -0.5], [-2.8 2.6; 0 -2.6; 2.8 1.6])
@@ -20,19 +38,25 @@ function quick_setup(agent_ids, agent_conns, nᵩ; testing=true)
     end
     δ_w = 0.001 # represents temporal system dynamics (shift from A=identity)
 
-    gt_params=LGSFModelParameters(μ=μ,σ=[1.],τ=[1.],ϕ₀=ϕ₀,
+    gt_params=LGSFModelParameters(μ=μ,σ=[σ],τ=[τ],ϕ₀=ϕ₀,
                                   A=Matrix{Float64}(I(nᵩ) .* (1- δ_w)),
                                   Q=0.000001*Matrix{Float64}(I(nᵩ)))
     gt_model=[initialize_SCRIBEModel_from_parameters(gt_params)]
 
+    if !ag_desc[:gt_same]
+        nᵩ = ag_desc[:nᵩ]
+        σ = ag_desc[:σ]
+        τ = ag_desc[:τ]
+        μ = ag_desc[:μ]
+    end
     ng = init_network_graph(agent_conns)
     a = 0
     for aid in agent_ids
         a += 1
-        ag_params=LGSFModelParameters(μ=μ,σ=[1.],τ=[1.],
+        ag_params=LGSFModelParameters(μ=μ,σ=[σ],τ=[τ],
                                       ϕ₀=zeros(nᵩ), A=Matrix{Float64}(I(nᵩ)),
                                       Q=0.0001*Matrix{Float64}(I(nᵩ)))
-        observer=LGSFObserverBehavior(0.1)
+        observer=LGSFObserverBehavior(ag_desc[:oₙ])
         init_agent_loc=[0. 0.; -0.5 -0.5] + [0. 0.; a-1 a-1]
         lg_Fs = initialize_KF(ag_params, observer, copy(init_agent_loc), gt_model[1])
         ng.vertices[aid] = initialize_agent(aid, lg_Fs, ng)
@@ -81,55 +105,6 @@ function generate_sample_locs_from_wpts(wpts::Vector, k::Integer, d::Float64,)
     end
 end
 
-function mul_agent_perf_conn(run_name::String, nₐ=3, nₛ=100; testing=true, tol=0.1)
-    space_corners = [-2., 2.] # corner coordinates
-
-    agent_ids = ["agent1", "agent2", "agent3", "agent4", "agent5"][1:nₐ]
-    if nₐ==3
-        agent_conns = Dict([("agent1", ["agent2"]),
-                            ("agent2", ["agent1", "agent3"]),
-                            ("agent3", ["agent2"])])
-    elseif nₐ==5
-        agent_conns = Dict([("agent1", ["agent2", "agent3"]),
-                            ("agent2", ["agent1", "agent3", "agent4"]),
-                            ("agent3", ["agent1", "agent2"]),
-                            ("agent4", ["agent2", "agent5"]),
-                            ("agent5", ["agent4"])])
-    else
-        return "Wrong number of agents champ."
-    end
-    nᵩ = 2
-    (gt_model, ng) = quick_setup(agent_ids, agent_conns, nᵩ; testing=testing)
-
-    agent_wpts = generate_agent_wpts(agent_ids, space_corners)
-    sample_dists = (space_corners[2] - space_corners[1])/20 # ensure distance is small enough for lawnmower pattern
-
-    for i in 1:nₛ
-        print("k: ", i)
-        while true
-            if all(map(aid->distributed_fusion(i, aid, ng, 0.1, 360), agent_ids))
-                print(" ...convergence reached:: ")
-                break
-            end
-        end
-
-        for aid in agent_ids; full_reset_network_connector(ng.vertices[aid].net_conn); end
-
-        push!(gt_model, update_SCRIBEModel(gt_model[i]))
-        for aid in agent_ids
-            progress_agent_env_filter(ng.vertices[aid].agent, gt_model[i+1],
-                                      copy(generate_sample_locs_from_wpts(agent_wpts[aid], i, sample_dists)))
-            push!(ng.vertices[aid].history, ng.vertices[aid].agent.estimates[i+1].observations.X)
-        end
-        println("ϕ: ", gt_model[end].ϕ, " | ̂ϕ: ", ng.vertices["agent1"].agent.estimates[end].estimate.ϕ)
-    end
-
-    simple_print_results(gt_model, ng)
-
-    @save "test/res_data/"*run_name*".jld2" gt_model ng
-    return gt_model, ng
-end
-
 function distance_based_conn(ng, agent_ids, conn_dist, i)
     new_conns = Dict{String, Vector{String}}()
     for ag1 in agent_ids
@@ -151,35 +126,44 @@ function no_comm_conns(ng, agent_ids)
     new_conns
 end
 
+function comm_conns(ng, agent_ids, conn_dist, i; comm_type=:dist)
+    @match comm_type begin
+        :dist => distance_based_conn(ng, agent_ids, conn_dist, i)
+        :none => no_comm_conns(ng, agent_ids)
+    end
+end
 
-function mul_agent_poor_conn(run_name::String, nₐ=3, nₛ=100; testing=true, tol=0.1, conn_dist=10, comm_type=:dist)
-    space_corners = [-5., 5.] # corner coordinates
+function single_run(run_name::String, gt_desc::Dict, ag_desc::Dict;
+                    nₛ=100, space_corners = [-5., 5.], conn_dist=10, comm_type=:dist)
+    nₐ = ag_desc[:nₐ]
 
-    agent_ids = ["agent1", "agent2", "agent3", "agent4", "agent5"][1:nₐ]
+    agent_ids = ["agent"*i for i in 1:nₐ]
+    agent_conns = @match nₐ begin
+        2 => Dict([("agent1", ["agent2"]),
+                   ("agent2", ["agent1"])])
+        3 => Dict([("agent1", ["agent2"]),
+                   ("agent2", ["agent1", "agent3"]),
+                   ("agent3", ["agent2"])])
+        4 => Dict([("agent1", ["agent2", "agent3"]),
+                   ("agent2", ["agent1", "agent4"]),
+                   ("agent3", ["agent1", "agent4"]),
+                   ("agent4", ["agent2", "agent3"])])
+        5 => Dict([("agent1", ["agent2", "agent3"]),
+                   ("agent2", ["agent1", "agent3", "agent4"]),
+                   ("agent3", ["agent1", "agent2"]),
+                   ("agent4", ["agent2", "agent5"]),
+                   ("agent5", ["agent4"])])
+    end
     if nₐ==3
-        agent_conns = Dict([("agent1", ["agent2"]),
-                            ("agent2", ["agent1", "agent3"]),
-                            ("agent3", ["agent2"])])
-    elseif nₐ==5
-        agent_conns = Dict([("agent1", ["agent2", "agent3"]),
-                            ("agent2", ["agent1", "agent3", "agent4"]),
-                            ("agent3", ["agent1", "agent2"]),
-                            ("agent4", ["agent2", "agent5"]),
-                            ("agent5", ["agent4"])])
-    else
         return "Wrong number of agents champ."
     end
-    nᵩ = 3
-    (gt_model, ng) = quick_setup(agent_ids, agent_conns, nᵩ; testing=testing)
+    (gt_model, ng) = quick_setup(agent_ids, agent_conns; gt_desc=gt_desc, ag_desc=ag_desc)
 
     agent_wpts = generate_agent_wpts(agent_ids, space_corners)
     sample_dists = (space_corners[2] - space_corners[1])/20 # ensure distance is small enough for lawnmower pattern
 
     for i in 1:nₛ
-        new_conns = @match comm_type begin
-            :dist => distance_based_conn(ng, agent_ids, conn_dist, i)
-            :none => no_comm_conns(ng, agent_ids)
-        end
+        new_conns = comm_conns(ng, agent_ids, conn_dist, i; comm_type=comm_type)
         update_network_graph_edges(new_conns, ng)
 
         print("k: ", i)
@@ -204,29 +188,15 @@ function mul_agent_poor_conn(run_name::String, nₐ=3, nₛ=100; testing=true, t
     simple_print_results(gt_model, ng)
 
     @save "test/res_data/"*run_name*".jld2" gt_model ng
-    return gt_model, ng
-end
-
-function simple_print_results(gt_model::Vector{T} where T<:SCRIBEModel, ng::NetworkGraph)
-    agent_ids = collect(keys(ng.vertices))
-
-    fests_m = [(aid*"m", ng.vertices[aid].agent.estimates[end].estimate.ϕ) for aid in agent_ids] # Final Mean Estimates
-    fests_v = [(aid*"v", inv(ng.vertices[aid].agent.information[end].Y)) for aid in agent_ids] # Final Variance Estimates
-    fvals = Dict([(:k, ng.vertices["agent1"].agent.k), (:ϕ, gt_model[end].ϕ), fests_m..., fests_v...]) # collate
-
-    println("Results:")
-    println("k: ", fvals[:k], "\nϕ(t=final):  ", fvals[:ϕ])
-    for aid in agent_ids
-        println("ϕ_", aid,"(t=final): ", fvals[aid*"m"]," | P_"*aid*"(t_final): ", fvals[aid*"v"])
-    end
+    return gt_model, ng, space_corners
 end
 
 function frechet_dist_eval_plot(run_name::String; layout_size=(800,500))
     png_name = "test/res_plots/"*run_name*"_frechet_dist.png"
 
-    @load "test/res_data/"*run_name*".jld2" gt_model ng
+    @load "test/res_data/"*run_name*".jld2" gt_model ng space_corners
 
-    x_range = -5:0.1:5
+    x_range = space_corners[1]:0.1:space_corners[2]
     y_range = copy(x_range)
     err_sq(gt, am, v) = norm(predict_SCRIBEModel(gt, v) - predict_SCRIBEModel(am, v))^2
     frechet(gt, am) = sum([err_sq(gt, am, [x, y]) for x in x_range for y in y_range])
@@ -242,8 +212,7 @@ function frechet_dist_eval_plot(run_name::String; layout_size=(800,500))
     p2 = plot(size=layout_size, xlabel="Time (in discretized steps)",
               title="Close-up of performance of agent learned environment models \nafter gathering initial observations", margin=(10, :mm))
     for aid in agent_ids
-        plot!(p2, (1:length(gt_model))[3:end], frechet_dists[aid][3:end], label="Agent "*aid[end], lw=2)
-        println(aid*" FDists: ", frechet_dists[aid])
+        plot!(p2, 1:length(gt_model), frechet_dists[aid], label="Agent "*aid[end], lw=2, yrange=[0., 1.0])
     end
 
     plot(p1, p2, layout=(1,2), size=(layout_size[1]*2, layout_size[2]))
@@ -261,7 +230,7 @@ end
 function error_map_plots(run_name::String; layout_size=(2700,1500), mode=:tane)
     png_name = "test/res_plots/"*run_name*"_err_map.png"
 
-    @load "test/res_data/"*run_name*".jld2" gt_model ng
+    @load "test/res_data/"*run_name*".jld2" gt_model ng space_corners
     gt = gt_model[end]
 
     num_plots = length(ng.vertices)
@@ -269,10 +238,9 @@ function error_map_plots(run_name::String; layout_size=(2700,1500), mode=:tane)
     # layout_num = (2,Integer(ceil(num_plots/2)))
     layout_num = (num_plots, num_plots+1)
     pred_cgrad = :thermal
-    gt_err_cgrad = :solar
-    rel_err_cgrad = :ice
+    err_cgrad = :ice
 
-    x_range = -5:0.1:5
+    x_range = space_corners[1]:0.1:space_corners[2]
     y_range = copy(x_range)
     gt_map = heatmap(x_range, y_range, [predict_SCRIBEModel(gt, [x,y]) for y in y_range, x in x_range],
                      color=:viridis, # xlabel="X", ylabel="Y",
@@ -299,7 +267,7 @@ function error_map_plots(run_name::String; layout_size=(2700,1500), mode=:tane)
                     push!(error_maps[aid], heatmap(x_range, y_range, [error_mapf(gt, m1, [x,y]; mode=mode) for y in y_range, x in x_range],
                                                    color=:viridis, # xlabel="X", ylabel="Y",
                                                    title="% error between \nAgent "*aid[end]*"'s predictions and ground truth",
-                                                   c = gt_err_cgrad))
+                                                   c = err_cgrad))
                     plot!(error_maps[aid][end], copy(h1[:,1]), copy(h1[:,2]),
                           linestyle=:dash, marker=:xcross, linecolor=:red, label="Agent "*aid[end]*" sampling sites")
                 else
@@ -307,11 +275,11 @@ function error_map_plots(run_name::String; layout_size=(2700,1500), mode=:tane)
                     push!(error_maps[aid], heatmap(x_range, y_range, [error_mapf(m1, m2, [x,y]; mode=mode) for y in y_range, x in x_range],
                                                    color=:viridis, # xlabel="X", ylabel="Y",
                                                    title="% error between \nAgent "*aid[end]*" and Agent "*a2d[end]*" predictions",
-                                                   c = rel_err_cgrad))
-                    # plot!(error_maps[aid][end], copy(h1[:,1]), copy(h1[:,2]),
-                    #       linestyle=:dash, marker=:xcross, linecolor=:red, label="Agent "*aid[end]*" sampling sites")
-                    # plot!(error_maps[aid][end], copy(h2[:,1]), copy(h2[:,2]),
-                    #       linestyle=:dash, marker=:xcross, linecolor=:red, label="Agent "*a2d[end]*" sampling sites")
+                                                   c = err_cgrad))
+                    plot!(error_maps[aid][end], copy(h1[:,1]), copy(h1[:,2]),
+                          linestyle=:dash, marker=:xcross, linecolor=:red, label="Agent "*aid[end]*" sampling sites")
+                    plot!(error_maps[aid][end], copy(h2[:,1]), copy(h2[:,2]),
+                          linestyle=:dash, marker=:xcross, linecolor=:red, label="Agent "*a2d[end]*" sampling sites")
                 end
             end
         end
@@ -324,4 +292,32 @@ function error_map_plots(run_name::String; layout_size=(2700,1500), mode=:tane)
     println("Plot saved at "*png_name)
 end
 
-# mul_agent_distrib_KF();
+function run_scriptor()
+    gt_descs = [(:gsf, Dict([(:nᵩ, 3), (:τ, 1.), (:σ, 1.)]), "full"),
+                (:gsf, Dict([(:nᵩ, 3), (:τ, 0.5), (:σ, 1.)]), "weak"),
+                (:gsf, Dict([(:nᵩ, 3), (:τ, 1.), (:σ, 0.5)]), "small")]
+    ag_desc = [[(:gt_same, true), (:σ, 1.), (:τ, 1.), (:μ, [-0.5 0; 0. 0.; 0.5 0.]), (:nᵩ, 3)],
+               create_grid_gsf_μστ([-5., 5], 0.5; τ=1.0, σ=0.5)]
+
+    anums = 2:5
+    comm_types = [:none, :dist]
+    num_samples = 25:25:100
+    obs_noises = [(0.01, "low"), (0.05, "med"), (0.1, "high")]
+
+    for gtd in gt_descs
+        for agd in ag_desc
+            for obn in obs_noises
+                for anum in anums
+                    for ctype in comm_types
+                        for nₛ in num_samples
+                            ad = Dict(vcat(agd, ((:nₐ, anum), (:oₙ, obn[1]))))
+                            run_name = anum*"a_"*agd[]*"w_"*gtd[3]*"GT_"*string(ctype)*"c_"*obn[2]*"o_"*string(nₛ)*"s_lawnmower"
+                            single_run(run_name, gtd, ad; nₛ=nₛ, space_corners = [-5., 5.], conn_dist=10, comm_type=ctype)
+                            error_map_plots(run_name)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
